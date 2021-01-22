f69dc02299fe280ec6e5daa167c466f0acaad065
sultan lök <12542002.sultan.lok@gmail.com>
2021-01-22 16:01:26 +0300

    Add files via upload

---
+++ DictionaryClassDemo/MyDictionary.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DictionaryClassDemo
+{
+    class MyDictionary<TKey,TValue>
+    {
+        TKey[] k;
+        TValue[] v;
+       
+
+        public MyDictionary()
+        {
+            k = new TKey[0];
+            v = new TValue[0];
+        }
+        public void Add(TKey key,TValue value)
+        {
+            TKey[] TempKey = k;
+            TValue[] TempValue = v;
+            k = new TKey[k.Length + 1];
+            v = new TValue[v.Length + 1];
+            for (int i = 0; i < TempKey.Length; i++)
+            {
+                k[i] = TempKey[i];
+                v[i] = TempValue[i];
+            }
+            k[k.Length - 1] = key;
+            v[v.Length - 1] = value;
+            
+        }
+        public TKey[] Keys { 
+        get { return k; }
+        } 
+        public TValue[] Values { 
+        get { return v; }
+        }
+        public void Dict()
+        {
+
+            for (int i = 0; i < k.Length; i++)
+            {
+                Console.WriteLine("Key: " + k[i] + " Value: " + v[i]);
+            }
+            
+        }
+
+    }
+  
+}
---
+++ DictionaryClassDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DictionaryClassDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, dynamic> users = new MyDictionary<string, dynamic>();
+            users.Add("Ad", "Sultan");
+            users.Add("Id", 1453);
+
+            foreach (var item in users.Keys)
+            {
+                Console.WriteLine("Key: "+item);
+            }
+            foreach (var item in users.Values)
+            {
+                Console.WriteLine("Value: "+item);
+            }
+
+            users.Dict();
+        }
+    }
+}
